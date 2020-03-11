d9d28226a2452a232d6af77a4cd55def02a3700a
hgko1207 <khkkhk1207@gmail.com>
2020-03-11 10:32:52 +0900

    action and dispatch

--- 7.틱택토/Td.jsx
+++ 7.틱택토/Td.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { CLICK_CELL, CHANGE_TURN } from "./TicTacToe";
 
-const Td = () => {
+const Td = ({ rowIndex, cellIndex, cellData, dispatch }) => {
+  const onClickTd = useCallback(() => {
+    console.log(rowIndex, cellIndex);
+    dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
+    dispatch({ type: CHANGE_TURN });
+  }, []);
+
   return (
     <>
-      <td></td>
+      <td onClick={onClickTd}>{cellData}</td>
     </>
   );
 };
--- 7.틱택토/Tr.jsx
+++ 7.틱택토/Tr.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Td from "./Td";
 
-const Tr = ({ rowData }) => {
+const Tr = ({ rowData, rowIndex, dispatch }) => {
   return (
     <tr>
+      {/* {Array.from({ length: rowData.length }, td => (
+        <Td>{}</Td>
+      ))} */}
       {Array(rowData.length)
         .fill()
-        .map(td => (
-          <Td>{}</Td>
+        .map((td, i) => (
+          <Td rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]} dispatch={dispatch}>
+            {""}
+          </Td>
         ))}
     </tr>
   );
