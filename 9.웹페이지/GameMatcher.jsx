15f27dd5ad0964604ec4cf3a82cd4f2d8af160a7
hgko1207 <khkkhk1207@gmail.com>
2020-03-18 14:09:53 +0900

    hashRouter, withRouter

---
+++ 9.웹페이지/GameMatcher.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class GameMatcher extends Component {
+  render() {
+    return <div>게임매쳐</div>;
+  }
+}
+
+export default GameMatcher;
--- 9.웹페이지/Games.jsx
+++ 9.웹페이지/Games.jsx
@@ -3,22 +3,23 @@ import { BrowserRouter, HashRouter, Route, Link } from "react-router-dom";
 import NumberBaseball from "../3.숫자야구/NumberBaseballClass";
 import RSP from "../5.가위바위보/RSPClass";
 import Lotto from "../6.로또/LottoClass";
+import GameMatcher from "./GameMatcher";
 
 const Games = () => {
   return (
     <HashRouter>
       <div>
-        <Link to="/number-baseball">숫자야구</Link>
+        <Link to="/game/number-baseball">숫자야구</Link>
         &nbsp;
-        <Link to="/rock-scissors-paper">가위바위보</Link>
+        <Link to="/game/rock-scissors-paper">가위바위보</Link>
         &nbsp;
-        <Link to="/lotto-generator">로또생성기</Link>
+        <Link to="/game/lotto-generator">로또생성기</Link>
+        &nbsp;
+        <Link to="/game/index">게임 매쳐</Link>
       </div>
 
       <div>
-        <Route path="/number-baseball" component={NumberBaseball}></Route>
-        <Route path="/rock-scissors-paper" component={RSP}></Route>
-        <Route path="/lotto-generator" component={Lotto}></Route>
+        <Route path="/game/:name" component={GameMatcher}></Route>
       </div>
     </HashRouter>
   );
