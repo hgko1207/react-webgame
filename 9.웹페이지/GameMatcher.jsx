54f797cfbbd76230b9712f70a066d05816267514
hgko1207 <khkkhk1207@gmail.com>
2020-03-18 14:30:23 +0900

    location, match, history

--- 9.웹페이지/GameMatcher.jsx
+++ 9.웹페이지/GameMatcher.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import NumberBaseball from "../3.숫자야구/NumberBaseballClass";
+import RSP from "../5.가위바위보/RSPClass";
+import Lotto from "../6.로또/LottoClass";
 
 class GameMatcher extends Component {
   render() {
-    return <div>게임매쳐</div>;
+    const { match } = this.props;
+    if (match.params.name == "number-baseball") {
+      return <NumberBaseball />;
+    } else if (match.params.name == "rock-scissors-paper") {
+      return <RSP />;
+    } else if (match.params.name == "lotto-generator") {
+      return <Lotto />;
+    }
+    return <div>일치하는 게임이 없습니다.</div>;
   }
 }
 
--- 9.웹페이지/Games.jsx
+++ 9.웹페이지/Games.jsx
@@ -1,27 +1,23 @@
 import React from "react";
 import { BrowserRouter, HashRouter, Route, Link } from "react-router-dom";
-import NumberBaseball from "../3.숫자야구/NumberBaseballClass";
-import RSP from "../5.가위바위보/RSPClass";
-import Lotto from "../6.로또/LottoClass";
+
 import GameMatcher from "./GameMatcher";
 
 const Games = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div>
         <Link to="/game/number-baseball">숫자야구</Link>
         &nbsp;
         <Link to="/game/rock-scissors-paper">가위바위보</Link>
         &nbsp;
         <Link to="/game/lotto-generator">로또생성기</Link>
-        &nbsp;
-        <Link to="/game/index">게임 매쳐</Link>
-      </div>
+      </div> 
 
       <div>
         <Route path="/game/:name" component={GameMatcher}></Route>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
